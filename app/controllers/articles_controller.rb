b9523fcddfa225f49d0640994284d4407fadecb0
Daniel Newman <dwnewman78@gmail.com>
2019-08-12 16:28:28 -0500

    Cleaned up some code offenses

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ArticlesController < ApplicationController
-  def index; 
+  def index
     @articles = Article.all
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  root to: "articles#index"
+  root to: 'articles#index'
 
   resources :articles
 end
