96c38bb46cfd7ea5310b6d50d764ab77993cec00
Daniel Newman <dwnewman78@gmail.com>
2019-08-12 15:49:37 -0500

    Implemented listing articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 class ArticlesController < ApplicationController
-  def index; end
+  def index; 
+    @articles = Article.all
+  end
 
   def new
     @article = Article.new
