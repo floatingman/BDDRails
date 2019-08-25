a1ab2485321cabb8219d0297bd81b220361edd8a
Daniel Newman <dwnewman78@gmail.com>
2019-08-25 13:21:16 -0500

    Added exception handling for articles not found

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  rescue_from ActiveRecord::RecordNotFound, with: :resource_not_found
+
+  protected
+
+  def resource_not_found
+  end
 end
